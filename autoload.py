0c42668ed788ab37fa4422fa6e08ed3cb6b890f2
GabboClown <russogab269@gmail.com>
2024-10-07 20:02:46 +0200

    Created autoloader file

---
+++ autoload.py
@@ -0,0 +1,8 @@
+import os
+from Authenticator import Authenticator
+from dotenv import load_dotenv
+
+# Dependencies used in main file
+__all__ = ['load_dotenv', 'os', 'Authenticator']
+
+load_dotenv()
