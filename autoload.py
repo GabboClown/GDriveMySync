41d3a4efcd5b41edc0071d018b22a3eb4f550c17
GabboClown <russogab269@gmail.com>
2024-10-08 11:09:13 +0200

    Added ServiceContainer and DriveRepository to autoload.py

--- autoload.py
+++ autoload.py
@@ -1,8 +1,12 @@
-import os
+from App.ServiceContainer import ServiceContainer
 from App.Api.Authenticator import Authenticator
+from App.Repositories.DriveRepository import DriveRepository
+import os
 from dotenv import load_dotenv
 
 # Dependencies used in main file
-__all__ = ['load_dotenv', 'os', 'Authenticator']
+__all__ = ['load_dotenv', 'os', 'ServiceContainer', 'Authenticator', 'DriveRepository']
 
 load_dotenv()
+
+ServiceContainer.register("DriveRepository", DriveRepository)
