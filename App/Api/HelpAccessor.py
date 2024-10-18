3f7fb948dc3952301a4836771e8de941d48b8f8a
GabboClown <russogab269@gmail.com>
2024-10-18 17:19:41 +0200

    Created HelpAccessor class to access program help string.

---
+++ App/Api/HelpAccessor.py
@@ -0,0 +1,20 @@
+class HelpAccessor:
+    @staticmethod
+    def access() -> str:
+        return """
+usage: python GDriveMySync.py [OPTION]
+
+Available options:
+-h, -help                    Shows this message.
+-c, -config                  Starts config procedure.
+-sc, -seeSettings            Shows settings.
+
+Use cases:
+python GDriveMySync.py -h            Shows this message.
+python GDriveMySync.py -help         Shows this message.
+python GDriveMySync.py -c            Starts config procedure.
+python GDriveMySync.py -config       Starts config procedure.
+python GDriveMySync.py -s            Shows settings.
+python GDriveMySync.py -seeSettings  Shows settings.
+python GDriveMySync.py               Starts the program with settings in config.json.
+"""
