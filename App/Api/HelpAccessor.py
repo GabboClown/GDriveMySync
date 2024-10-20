114e0d9663b9dd6991fb7347738b173e1397fa90
GabboClown <russogab269@gmail.com>
2024-10-20 11:30:48 +0200

    Fixed help string in HelpAccessor.

--- App/Api/HelpAccessor.py
+++ App/Api/HelpAccessor.py
@@ -5,9 +5,9 @@ class HelpAccessor:
 usage: python GDriveMySync.py [OPTION]
 
 Available options:
--h, -help                    Shows this message.
--c, -config                  Starts config procedure.
--sc, -seeSettings            Shows settings.
+-h, -help                            Shows this message.
+-c, -config                          Starts config procedure.
+-s, -settings                        Shows settings.
 
 Use cases:
 python GDriveMySync.py -h            Shows this message.
@@ -15,6 +15,6 @@ python GDriveMySync.py -help         Shows this message.
 python GDriveMySync.py -c            Starts config procedure.
 python GDriveMySync.py -config       Starts config procedure.
 python GDriveMySync.py -s            Shows settings.
-python GDriveMySync.py -seeSettings  Shows settings.
+python GDriveMySync.py -settings     Shows settings.
 python GDriveMySync.py               Starts the program with settings in config.json.
 """
